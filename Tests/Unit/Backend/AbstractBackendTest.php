325cf6b268a71772b75ce4a07ca37fe0eacd8ad5
Karsten Dambekalns <karsten@dambekalns.de>
2026-08-11 14:55:10 +0200

    BUGFIX: Make nullable parameters explicitly nullable

--- Tests/Unit/Backend/AbstractBackendTest.php
+++ Tests/Unit/Backend/AbstractBackendTest.php
@@ -29,7 +29,7 @@ final class AbstractBackendTest extends UnitTestCase
         {
             protected $someOption;
             public function open(): void {}
-            public function append(string $message, int $severity = 1, $additionalData = NULL, string $packageKey = NULL, string $className = NULL, string $methodName = NULL): void {}
+            public function append(string $message, int $severity = 1, $additionalData = NULL, ?string $packageKey = NULL, ?string $className = NULL, ?string $methodName = NULL): void {}
             public function close(): void {}
             public function setSomeOption($value) {
                 $this->someOption = $value;
